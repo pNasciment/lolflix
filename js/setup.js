bef8d153d1e7b65fb47a6b74b8f33849f4fffe4a
pNasciment <p.fontes.nascimento@gmail.com>
2022-04-10 17:35:37 -0300

    primeiro push

---
+++ js/setup.js
@@ -0,0 +1,60 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:8
+        }
+    }
+})
+
+
+
+function newPopup(champ){
+    var name = champ.toString();
+    if (name == 'zed'){
+        let varJanela = window.open ('https://leagueoflegends.fandom.com/pt-br/wiki/Zed')
+    }else if(name== 'ahri'){
+        let varJanela = window.open ('https://leagueoflegends.fandom.com/pt-br/wiki/Ahri')
+    }else if(name== 'katarina'){
+        let varJanela = window.open ('https://leagueoflegends.fandom.com/pt-br/wiki/Katarina')
+    }else if(name== 'mordekaiser'){
+        let varJanela = window.open ('https://leagueoflegends.fandom.com/pt-br/wiki/Mordekaiser')
+    }else if(name== 'morgana'){
+        let varJanela = window.open ('https://leagueoflegends.fandom.com/pt-br/wiki/Morgana')
+    }else if(name== 'veigar'){
+        let varJanela = window.open ('https://leagueoflegends.fandom.com/pt-br/wiki/Veigar')
+    }else if(name== 'yasuo'){
+        let varJanela = window.open ('https://leagueoflegends.fandom.com/pt-br/wiki/Yasuo')
+    }else if(name== 'fizz'){
+        let varJanela = window.open ('https://leagueoflegends.fandom.com/pt-br/wiki/Fizz')
+    }else if(name== 'viktor'){
+        let varJanela = window.open ('https://leagueoflegends.fandom.com/pt-br/wiki/Viktor')
+    }else if(name== 'fiora'){
+        let varJanela = window.open ('https://leagueoflegends.fandom.com/pt-br/wiki/Fiora')
+    }else if(name== 'jax'){
+        let varJanela = window.open ('https://leagueoflegends.fandom.com/pt-br/wiki/Jax')
+    }else if(name== 'nasus'){
+        let varJanela = window.open ('https://leagueoflegends.fandom.com/pt-br/wiki/Nasus')
+    }else if(name== 'renekton'){
+        let varJanela = window.open ('https://leagueoflegends.fandom.com/pt-br/wiki/renekton')
+    }else if(name== 'rengar'){
+        let varJanela = window.open ('https://leagueoflegends.fandom.com/pt-br/wiki/rengar')
+    }else if(name== 'riven'){
+        let varJanela = window.open ('https://leagueoflegends.fandom.com/pt-br/wiki/riven')
+    }else if(name== 'volibear'){
+        let varJanela = window.open ('https://leagueoflegends.fandom.com/pt-br/wiki/volibear')
+    }else if(name== 'viktor'){
+        let varJanela = window.open ('https://leagueoflegends.fandom.com/pt-br/wiki/Viktor')
+    }else if(name== 'viktor'){
+        let varJanela = window.open ('https://leagueoflegends.fandom.com/pt-br/wiki/Viktor')
+    }
+    
+}
